21ee7ed2df930bc22ad246482c7f19f9fcfbafc0
Ashura2003 <shreejanm21@gmail.com>
2024-05-04 12:28:20 +0545

    fixed error

--- index.js
+++ index.js
@@ -3,15 +3,14 @@ const app = express();
 const dotenv = require("dotenv");
 const connectDatabase = require("./database/database");
 
-app.use("/api/", require("./routes/userRoutes"));
-
 
 dotenv.config();
 
 connectDatabase();
+app.use(express.json());
 
 const PORT = process.env.PORT;
-app.use(express.json());
+app.use("/api/", require("./routes/userRoutes"));
 
 app.use("/api/", require("./routes/reservationRoute"));
 app.use("/api/", require("./routes/bookAppointmentRoute"));
